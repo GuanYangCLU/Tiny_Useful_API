b1f1e1ce3fd2cc740fe12c5829be99395f6b3e09
GuanYangCLU <guanyang@callutheran.edu>
2020-07-04 03:16:13 -0700

    calendar month 5

--- js/index/schedule.js
+++ js/index/schedule.js
@@ -5,10 +5,11 @@ function changeMonth(month) {
 
 function initMonth(month) {
   console.log("init month called, month: ", month);
-  $("#month").html(getScheduleTitle(month));
+  $("#month").html(getSchedule(month).title);
+  $("#schedule").html(getSchedule(month).content);
 }
 
-function getScheduleTitle(month) {
+function getSchedule(month) {
   console.log("get schedule title called, month: ", month);
   var date = new Date();
   var curYear = date.getFullYear();
@@ -39,7 +40,70 @@ function getScheduleTitle(month) {
       // TODO: input number of month
     }
   }
-  return curYear + " - " + curMonth;
+  return {
+      title: curYear + " - " + curMonth,
+      content: getContent({ year: curYear, month: curMonth });
+    };
+}
+
+function initSchedule(month) {
+    console.log("init schedule called, month: ", month);
+    $("#schedule").html(getSchedule(month).content);
+}
+
+function getContent({ year, month }) {
+    $.getJSON('js/leetCodeInfo.json', function (data) {
+        var item = data[year][month - 1]; // arr with month day data
+        var firstDateTime = year + '-' + month + '-01 00:01';
+        var firstDay = (new Date(firstDateTime)).getDay();
+        for (var i=0; i<firstDay; i++) {
+            item.unshift('');
+        }
+        var tail = 7 - (item.length % 7);
+        for (var j = 0; j < tail; j++) {
+            item.push('');
+        }
+        // maybe 28 or 35 or 42 length
+        return getMonthContent(item);
+    })
+}
+
+function getMonthContent(item) {
+    var template = '';
+    var weeks = item.length / 7;
+    for (var i = 0; i < weeks; i++) {
+        weekItem = item.slice(i * 7, (i+1) * 7);
+        template += ('<tr>' + getWeekContent(weekItem) + '</tr>');
+    }
+    return template;
+}
+
+function getWeekContent(weekItem) {
+    var template = '';
+    // { checeked, date, notes: [ { content } ] }
+    for (var i = 0; i < 7; i++) {
+        if (!weekItem[i]) {
+            // not in this month, item is ''
+            template += ('<td class=\"date unavailable\"></td>');
+        } else {
+            template += ('<td class=\"date'
+                + (weekItem[i].checked === true ? ' checked' : '')
+                + '\">'
+                + getDayContent(weekItem[i])
+                + '</td>');
+        }
+    }
+    return template;
+}
+
+function getDayContent(dayItem) {
+    var dateField = '<div class=\"dateNum\">' + dayItem.date + '</div>';
+    var notesField = '';
+    for (var i = 0; i < dayItem.notes.length; i++) {
+        // note may include content, color, id, level ...
+        notesField += ('<div class=\"notes\">' + dayItem.notes[i].content + '</div>');
+    }
+    return dateField + notesField;
 }
 
 // https://blog.csdn.net/weixin_42476601/article/details/81196490
@@ -47,3 +111,5 @@ function getScheduleTitle(month) {
 $(document).ready(function () {
   initMonth();
 });
+
+// TODO: string number check
